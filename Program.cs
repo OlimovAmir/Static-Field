2a217260e70ed5edfdd281d377314cda93ff234f
olimov Amir <olimov.amir@mail.ru>
2023-12-24 11:07:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+class MyClass
+{
+    public int a;
+    public static int b;
+}
+
+class MyClass2
+{
+    private static int b;
+
+    public void SetB(int b)
+    {
+        MyClass2.b = b;
+    }
+
+    public void PrintB()
+    {
+        Console.WriteLine(b);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass.b = 15;
+        MyClass myClass = new MyClass();
+        myClass.a = 44;
+
+        MyClass myClass2 = new MyClass();   
+        myClass2.a = 22;   
+    }
+}
